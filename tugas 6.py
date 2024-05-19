fb96132e948a8b1844cdb510e3a7ef65b25a7919
Enengsalwa <160403792+Enengsalwa@users.noreply.github.com>
2024-05-19 21:28:08 +0700

    Add files via upload

---
+++ tugas 6.py
@@ -0,0 +1,7 @@
+def hitungkata(kalimat):
+    kata_kata = kalimat.split()
+    jumlahkata = len(kata_kata)
+    return jumlahkata
+
+kalimat = "Ini adalah contoh kalimat"
+print(hitungkata(kalimat))
